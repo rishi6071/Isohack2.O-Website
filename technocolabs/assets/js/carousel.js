529c398615597560b8d24143233283942ec3d7df
rishi6071 <52880158+rishi6071@users.noreply.github.com>
2020-07-24 21:43:16 +0530

    Project In Progress

---
+++ technocolabs/assets/js/carousel.js
@@ -0,0 +1,15 @@
+
+// TESTIMONIAL
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1300:{
+            items:1
+        }
+    }
+  })

1db51216b30d686e1698dcaa5f84c71c1480adda
rishi6071 <52880158+rishi6071@users.noreply.github.com>
2020-07-31 01:35:37 +0530

    Project Initialised

---
+++ coffeeking/assets/js/custom_js/custom.js
@@ -0,0 +1,13 @@
+// Custom
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $(".dropdown-trigger").dropdown();
+  $(".slider").slider();
+  $('.sidenav').sidenav();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    duration: 200
+  });
+  $('.parallax').parallax();
+});

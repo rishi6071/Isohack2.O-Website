0a6092ed3c51f2cbd3299c3ffbf6ae0a9bb45239
rishi6071 <52880158+rishi6071@users.noreply.github.com>
2020-08-06 16:37:58 +0530

    Project Initialised

--- coffeeking/assets/js/custom_js/custom.js
+++ coffeeking/assets/js/custom_js/custom.js
@@ -1,20 +1,25 @@
 // -------> Custom
 $(document).ready(function () {
+
   $('.sidenav').sidenav();
   $(".dropdown-trigger").dropdown();
   $(".slider").slider();
   $('.sidenav').sidenav();
+  $('.parallax').parallax();
+  $('.fixed-action-btn').floatingActionButton();
+  $('.materialboxed').materialbox();
+
   $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true,
     duration: 200
   });
-  $('.parallax').parallax();
+
   $('.carousel').carousel({
     numVisible: 7,
     shift: 40,
     padding: 40
   });
-  $('.fixed-action-btn').floatingActionButton();
-  $('.materialboxed').materialbox();
+
+  $('textarea#message').characterCounter();
 });
